c27a126e051666cd3a73789583778da9e2859dd4
Tam Nguyen <tamnguyenit@users.noreply.github.com>
2017-04-24 10:18:41 +0800

    rewrite using prepend

--- lib/cacheable/cacheable.rb
+++ lib/cacheable/cacheable.rb
@@ -42,88 +42,67 @@ module Cacheable
     class_eval do
       def self.caches_method(*names)
         opts = names.extract_options!
-        @cached_methods ||= []
-        @cached_methods |= names
-
-        @options ||= {}
+        @cache_options ||= {}
         names.each do |name|
-          @options[name] = opts
-          # if instance_methods.include? name.to_sym
-          #   add_method_with_cache name
-          # elsif methods.include? name.to_sym
-          #   add_singleton_method_with_cache name
-          # else
-          #   @cached_methods |= [name]
-          # end
+          @cache_options[name] = opts
+          add_method_with_cache name
         end
       end
 
-      def self.caches_extended_method(*names)
+      def self.caches_class_method(*names)
         opts = names.extract_options!
-        @options ||= {}
+        @cache_options ||= {}
         names.each do |name|
-          @options[name] = opts
-          add_singleton_method_with_cache name
+          @cache_options[name] = opts
+          add_class_method_with_cache name
         end
-      end
-
-      def self.cacheable?(name)
-        @cached_methods.present? && @cached_methods.delete(name).present?
-      end
-
-      def self.method_added(name)
-        super
-        add_method_with_cache name if cacheable? name
-      end
-
-      def self.singleton_method_added(name)
-        super
-        add_singleton_method_with_cache name if cacheable? name
       end
 
       private
 
       def self.add_method_with_cache(name)
-
-            p "ADDD add_method_with_cache #{name}"
-        class_eval(generate_method_with_cache(name), __FILE__, __LINE__ + 1)
-        alias_method_chain name, :cache
+        prepend cache_module_for(name)
       end
 
-      def self.add_singleton_method_with_cache(name)
-            p "ADDD add_singleton_method_with_cache #{name}"
-        generated_method = generate_method_with_cache(name)
-        singleton_class.instance_eval do
-          class_eval(generated_method, __FILE__, __LINE__ + 1)
-          alias_method_chain name, :cache
-        end
+      def self.add_class_method_with_cache(name)
+        singleton_class.prepend cache_module_for(name)
       end
 
-      def self.generate_method_with_cache(target)
-        options = @options[target]
+      def self.cache_module_for(target)
+        options = @cache_options[target]
         duration = options.delete(:expires_in)
         duration ||= Cacheable.default_cache_duration
         name = target.to_s.sub(/([?!=])$/, '')
         punctuation = Regexp.last_match(1)
 
-        <<-EVAL
-        def #{name}_with_cache#{punctuation}(*args)
-          key = Cacheable.cache_key(self, '#{name}', *args#{", #{options}" unless options.empty?})
-          #{generate_request_store(target)} Rails.cache.fetch(key, expires_in: #{duration}) do
-            #{name}_without_cache#{punctuation}(*args)
+        cache_module = Module.new do
+          define_method(target) do |*args|
+            key = Cacheable.cache_key(self, target, *args, options)
+            generate_request_store(key, options) do
+              Rails.cache.fetch(key, expires_in: duration) do
+                super(*args)
+              end
+            end
+          end
+
+          define_method("delete_#{name}_cache#{punctuation}") do |*args|
+            key = Cacheable.cache_key(self, target, *args, options)
+            RequestStore.store[key.to_sym] = nil
+            Rails.cache.delete(key)
+          end
+
+                         private
+
+          def generate_request_store(key, options)
+            if options && options[:memoized] == true
+              RequestStore.store[key.to_sym] ||= yield
+            else
+              yield
+            end
           end
         end
 
-        def delete_#{name}_cache#{punctuation}(*args)
-          key = Cacheable.cache_key(self, '#{name}', *args#{", #{options}" unless options.empty?})
-          RequestStore.store[key.to_sym] = nil
-          Rails.cache.delete(key)
-        end
-        EVAL
-      end
-
-      def self.generate_request_store(target)
-        'RequestStore.store[key.to_sym] ||=' if @options[target][:memoized] == true
+        const_set("#{name}_cache".camelize, cache_module)
       end
     end
   end
